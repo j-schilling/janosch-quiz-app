d1fe9a01a5be3fa4ca90a59f180d21ea3b2c53fc
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-18 12:43:27 +0100

    enabling show answer button

--- js/index.js
+++ js/index.js
@@ -3,7 +3,21 @@ console.clear();
 const main = document.querySelector('[data-js="main"]');
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const bookmark = document.querySelector('[data-js="bookmark"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+let isAnswerButtonClicked = false;
 
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("bookmark__filled");
+});
+
+answerButton.addEventListener("click", () => {
+  isAnswerButtonClicked = !isAnswerButtonClicked;
+  isAnswerButtonClicked
+    ? answer.removeAttribute("hidden")
+    : answer.setAttribute("hidden", true);
+
+  isAnswerButtonClicked
+    ? (answerButton.textContent = "Hide answer")
+    : (answerButton.textContent = "Show answer");
 });
